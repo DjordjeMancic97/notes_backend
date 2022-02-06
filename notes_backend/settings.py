2d007c6a42fc08c669748155844836cdf7f6c026
DjordjeMancic97 <mancicdjordjeee@gmail.com>
2022-02-06 19:32:49 +0100

    remove key

--- notes_backend/settings.py
+++ notes_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ph-iduoraha_1tzc-lo*9u$b&zo-k2d9*7i(w!384zalgwuolh'
+# SECRET_KEY = 'django-insecure-ph-iduoraha_1tzc-lo*9u$b&zo-k2d9*7i(w!384zalgwuolh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
